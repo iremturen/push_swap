090c3868e45452f759943a6fbae03998365d4edc
İrem Türen <72929400+iremturen@users.noreply.github.com>
2025-10-17 23:19:03 +0300

    sort five

--- small_stack_sort.c
+++ small_stack_sort.c
@@ -18,6 +18,24 @@ int	get_max(t_stack *a)
 	return (max);
 }
 
+int	get_min(t_stack *a)
+{
+	int		min;
+	t_stack	*temp;
+
+	if (!a)
+		return (-1);
+	min = a->index;
+	temp = a->next;
+	while (temp)
+	{
+		if (temp->index < min)
+			min = temp->index;
+		temp = temp->next;
+	}
+	return (min);
+}
+
 void	sort_three(t_stack **a, t_stack **b)
 {
 	int	max;
@@ -40,10 +58,30 @@ void	sort_three(t_stack **a, t_stack **b)
 	pa(a, b);
 }
 
+void	sort_five(t_stack **a, t_stack **b)
+{
+	int	min1;
+	int	min2;
+
+	min1 = get_min(*a);
+	while ((*a)->index != min1)
+		rotate_stack(a, "ra\n");
+	pb(a, b);
+	min2 = get_min(*a);
+	while ((*a)->index != min2)
+		rotate_stack(a, "ra\n");
+	pb(a, b);
+	sort_three(a, b);
+	pa(a, b);
+	pa(a, b);
+}
+
 void	small_stack_sort(t_stack **a, t_stack **b, int size)
 {
 	if (size == 2)
 		swap_stack(a, "sa\n");
 	else if (size == 3)
 		sort_three(a, b);
+	else if (size == 5)
+		sort_five(a, b);
 }
